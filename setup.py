eee9e266e099d3a4b8a1f46fd138a7b4c21a66bf
Philip Horger <campadrenalin@gmail.com>
2013-03-12 23:25:31 -0700

    Second attempt at tox support, including the .c file

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from distutils.core import setup
 from distutils.extension import Extension
-try:
-    from Cython.Distutils import build_ext
-except:
-    # Buys us that first round we need to state a cython dependency
-    build_ext = None
 
 ext_modules=[
     Extension('dogfood',
-        ["dogfood.pyx"]
+        ["dogfood.c"]
         )
     ]
 
@@ -21,7 +16,6 @@ setup(
     name="dogfood",
     description="Serialize custom classes with JSON",
     version="0.0.2",
-    cmdclass = {"build_ext": build_ext},
     ext_modules = ext_modules,
     author="Derek Arnold",
     author_email="derek@brainindustries.com",
